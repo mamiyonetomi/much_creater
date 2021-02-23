d8d6290f9d88fc99ad68914de465b34c90c26c4c
yonetomi <mamiyonetomi0331@gmail.com>
2021-02-23 22:35:17 +0900

    修正

--- database/seeds/ShopsTableSeeder.php
+++ database/seeds/ShopsTableSeeder.php
@@ -19,5 +19,6 @@ class ShopsTableSeeder extends Seeder
         Shop::create(['name' => 'あいおえお工房　福岡店','addressnumber' => '812-0012','address1' => '福岡県','address2' => '福岡市博多区','address3' => '博多駅中央街9-1','tell' => '092-577-1611','hp' => 'https://google.com','img1' => 'hakata1.jpg','img2' => 'hakata2.jpg','img3' => 'hakata3.jpg','detail' => 'お店の詳細説明です。','delivery' => 1]);
         Shop::create(['name' => 'いろはにほへと　天神店','addressnumber' => '810-0001','address1' => '福岡県','address2' => '福岡市中央区','address3' => '天神1-7-11','tell' => '092-731-3909','hp' => 'https://google.com','img1' => 'tenjin1.jpg','img2' => 'tenjin2.jpg','img3' => 'tenjin3.jpg','detail' => 'お店の詳細説明です。','delivery' => 0]);    
         Shop::create(['name' => '港工房　横浜店','addressnumber' => '220-0045','address1' => '神奈川県','address2' => '横浜市西区','address3' => '伊勢町1-74','tell' => '045-286-0112','hp' => 'https://google.com','img1' => 'yokohama1.jpg','img2' => 'yokohama2.jpg','img3' => 'yokohama3.jpg','detail' => 'お店の詳細説明です。','delivery' => 1]);
+        Shop::create(['name' => '〇〇〇工房　福岡店','addressnumber' => '814-0104','address1' => '福岡県','address2' => '福岡市中央区','address3' => '天神1-2-3','tell' => '092-123-4567','hp' => 'https://google.com','img1' => 'aaa.jpeg','img2' => 'bbb.jpeg','img3' => 'ccc.jpeg','detail' => 'お店の詳細説明です。','delivery' => 0]);
     }
 }
