001c49b16023ddcfeb8fd31d872e59fc486281ab
yonetomi <mamiyonetomi0331@gmail.com>
2021-02-23 13:29:03 +0900

    修正コメント１２

---
+++ database/seeds/ServicesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\shop;
+class ServicesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+    }
+}
